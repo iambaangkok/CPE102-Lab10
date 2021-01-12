b2064b46124dcadf1ab97d554d1e276d5a075195
Baangkok Vanijyananda <iambaangkok@gmail.com>
2021-01-12 15:17:49 +0700

    Lab10_1 completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,28 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+int main(){
+    srand(time(0)); 
+    cout << "Press Enter 3 times to reveal your future.";
+    string input = "";
+    int enter = 3;
+    while(enter){
+        getline(cin,input);
+        if(input == "") enter--;
+    }
+    int r = rand()%9+1;
+    string grade = (r==1)? "A": (r==2)? "B+": (r==3)? "B": (r==4)? "C+" : (r==5)? "C": 
+   (r==6)? "D+": (r==7)? "D": (r==8)? "F": (r==9)? "W":"W";
+
+    
+
+    cout << "You will get " << grade << " in this 261102.";
+
+    return 0;
+}
+
+
